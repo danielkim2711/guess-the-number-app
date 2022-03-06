2ceea7d65200eac69581e4e2489a01f0856a21f9
Daniel Kim <danielkim2711@gmail.com>
2022-03-07 10:58:22 +1300

    Define Header component

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screenView}>
+      <Header />
+      <StatusBar style='light' />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screenView: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
